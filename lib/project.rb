201eb561b9b25e148f6c228030bbb8fd4d6209fb
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-17 17:26:49 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,14 @@
 class Project
   
-attr_writer :backers
 attr_reader :title 
 
   def initialize (title)
     @title = title 
     @backers = []
+  end 
+  
+  def backers 
+    @backers = backers 
   end 
 
 end 
