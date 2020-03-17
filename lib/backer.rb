8950c46d8b2cfcefe42187eeb858f5a66277b2ed
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-17 17:16:46 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,14 +2,12 @@ class Backer
 attr_accessor 
 attr_reader :name, :backed_projects 
 
-backed_projects = []
 
 def initialize (name)
     @name = name 
 end
 
 def backed_projects 
-  @backed_projects = backed_projects
 end
 
 end 
